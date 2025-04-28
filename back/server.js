661edefdfbd5eb6be04d3ef0951c4fb7e4033504
TTH18ru <runi@yandex.ru>
2025-04-28 14:20:58 +0000

    need:fix qr, accaunt

--- back/server.js
+++ back/server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+const https = require('https');
+const fs = require('fs');
 const path = require('path');
+
+const options = {
+    key: fs.readFileSync(path.join(__dirname, 'server.key')), 
+    cert: fs.readFileSync(path.join(__dirname, 'server.crt'))
+};
 
 const app = express();
 const PORT = 3000;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../front')));
 
 // MySQL connection
 const db = mysql.createConnection({
@@ -30,31 +37,27 @@ db.connect((err) => {
 });
 // Serve the HTML form
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname+ '../'));
+    res.sendFile(path.join(__dirname, '../front/reg.html'));
 });
 
 // Handle form submission
 app.post('/submit', (req, res) => {
-    const { studentId, name, surname, dob, class: studentClass } = req.body;
-    const sql = 'INSERT INTO students (studentId, name, surname, dob, class) VALUES (?, ?, ?, ?, ?)';
-    db.query(sql, [studentId, name, surname, dob, studentClass], (err, result)  => {
-        if (err) {
-            return res.status(500).send('Error inserting data');
+    const { username, password, studentId, name, surname, dob, class: studentClass } = req.body;
+
+    const user = { username, password, studentId, name, surname, dob, class: studentClass };
+
+    db.query('INSERT INTO users SET ?', user, (error, results) => {
+        if (error) {
+            console.error('Error registering user:', error); // Логирование ошибок регистрации
+            return res.status(400).send('Error registering user');
         }
-        res.send('Data inserted successfully!');
+        res.status(201).send('User registered successfully');
     });
 });
 
-app.post('/submit-qr', (req, res) => {
-    const { studentId } = req.body; // Assuming the QR code contains the student ID
-    console.log('Scanned Student ID:', studentId);
-    
-    // Here you can handle the student ID, e.g., save it to a database or perform other actions
-
-    res.json({ message: 'Student ID received successfully!', studentId });
-});
 
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+
+ https.createServer(options, app).listen(PORT, '192.168.1.60', () => {
+        console.log(`Server is running on https://192.168.1.60:${PORT}`);
 });
